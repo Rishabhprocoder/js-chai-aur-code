45e24722b28d25edea69f571daa3a4e2f5556768
Rishabhprocoder <137711326+Rishabhprocoder@users.noreply.github.com>
2024-11-03 12:23:12 +0000

    variables knowledge

---
+++ 01_Jsbasics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 47498
+let accountemail= "rishabh@google.com"
+var accountpassword= "12345"
+accountcity= "Delhi"
+let accountstate;
+//accountId=2 not allowed because const can't be changed
+
+accountemail= "rs@yahoo.com"
+accountpassword= "54321"
+accountcity= "Gurugram";
+
+console.log(accountId);
+/*
+Prefer not to use var*/
+console.table([accountId, accountemail, accountpassword, accountcity, accountstate])
+
+
